a66430e7693d3271cf50c574bad9e22009f00d73
samuelshine <samuelshine112003@gmail.com>
2024-04-16 11:36:06 +0530

    Update Sidebar.jsx

--- frontend/src/Components/Sidebar/Sidebar.jsx
+++ frontend/src/Components/Sidebar/Sidebar.jsx
@@ -1,30 +1,40 @@
 import React from 'react';
 import './Sidebar.css';
-
 import { useNavigate } from 'react-router-dom';
 
 const Sidebar = () => {
-  let navigate = useNavigate(); 
+  let navigate = useNavigate();
 
   const createNewPost = () => {
     navigate('/create/post');
   };
 
+  const logout = () => {
+    localStorage.removeItem('username');
+    navigate('/login');
+  };
+
+  const goToProfile = () => {
+    const username = localStorage.getItem('username');
+    navigate(`/profile/${username}`);
+  };
+
   return (
     <div className='sidebar-container'>
-        <div className="sidebar-header">
-            <h1>Instagram</h1>
-        </div>
-        <div className="menu-items">
-            <div className="home">Home</div>
-            <div className="explore">Explore</div>
-            <div className="messages">Messages</div>
-            <div className="likes">Likes</div>
-            <div className="create" onClick={createNewPost}>Create</div>
-            <div className="profile">Profile</div>
-        </div>
+      <div className="sidebar-header">
+        <h1>Instagram</h1>
+      </div>
+      <div className="menu-items">
+        <div className="home">Home</div>
+        <div className="explore">Explore</div>
+        <div className="messages">Messages</div>
+        <div className="likes">Likes</div>
+        <div className="create" onClick={createNewPost}>Create</div>
+        <div className="profile" onClick={goToProfile}>Profile</div>
+        <div className="logout" onClick={logout}>Logout</div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Sidebar;
