8f773fa25a9b009501c4e239ad656a8900360af8
samuelshine <samuelshine112003@gmail.com>
2024-05-18 16:16:44 +0530

    tests fixes

--- Test/test_openSignup.py
+++ Test/test_openSignup.py
@@ -9,7 +9,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
-class TestViewProfile():
+class TestOpenSignup():
   def setup_method(self, method):
     self.driver = webdriver.Chrome()
     self.vars = {}
@@ -17,7 +17,7 @@ class TestViewProfile():
   def teardown_method(self, method):
     self.driver.quit()
 
-  def test_viewProfile(self):
+  def test_openSignup(self):
     self.driver.get("http://localhost:3000/login")
     self.driver.set_window_size(1200, 762)
     self.driver.find_element(By.LINK_TEXT, "New here? Create an account").click()
